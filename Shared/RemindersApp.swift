8ce207ed3e9128845d9577e436842261a005b71b
Mo Asche <masche@lcs.on.ca>
2022-01-25 08:54:00 -0500

    Initial Commit

---
+++ Shared/RemindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersApp.swift
+//  Shared
+//
+//  Created by Moritz Conrad Asche on 2022-01-25.
+//
+
+import SwiftUI
+
+@main
+struct RemindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
